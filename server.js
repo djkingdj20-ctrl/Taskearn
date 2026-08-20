4ed6ffe7dba85081f7698c32f3885955d6ed3848
djkingdj20-ctrl <djkingdj20@gmail.com>
2026-08-20 15:16:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -309,11 +309,16 @@ function safeCompare(a, b) {
 ===================================================== */
 
 const mailTransporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: GMAIL_USER,
     pass: GMAIL_APP_PASSWORD
-  }
+  },
+  connectionTimeout: 15000,
+  greetingTimeout: 15000,
+  socketTimeout: 15000
 });
 
 async function verifyMailConnection() {
